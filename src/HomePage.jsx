11c64944b2ac2ffb30487f1e2a0ce789b6907529
Gurshan Singh Aulakh <shan.aulakh29@gmail.com>
2024-10-26 12:34:08 -0700

    completed the footer requirement by following all the acceptance requirements

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './Header.jsx'
 import HomePageText from './HomePageText.jsx'
-
+import Footer from './Footer.jsx'
 export default function HomePage() {
   return (
     <div>
          <Header/>
-        <HomePageText />
-     
+         <HomePageText />
+         <Footer/> 
     </div>
   )
 }
