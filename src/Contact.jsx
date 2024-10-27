07c42ba3233dfe5b5375950d42f660e94e8f4629
Gurshan Singh Aulakh <shan.aulakh29@gmail.com>
2024-10-27 13:11:04 -0700

    email.js is working and now i receivethe email when anyone presses submit button but the verifcation has yet to be done

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
+import emailjs from '@emailjs/browser'
 export default function Contact() {
+  const serviceId="service_pj179qr";
+  const templateId="template_6lcyfn1";
+  const publicKey="grVdEAxCPfWVNwLux";
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+    function handleSubmit(e){
+     e.preventDefault();
+     const templateParams={
+      from_name:name,
+      from_email:email,
+      message,
+      to_name: "Gurshan Aulakh"
+  
+    }
+    emailjs.send(serviceId, templateId,templateParams, publicKey).then((success)=>{
+      console.log("Email sent successfully "+success);
+      alert("Thank you, "+name+", for reaching out! I'll get back to you as soon as possible.")
+      setName('')
+      setEmail('')
+      setMessage('')
+    }).catch((error)=>{
+      console.log("Error sending email "+e.message);
+    })
+    }
   return (
     <div className="md:min-h-screen md:flex md:items-center md:justify-center">
       <div className=" flex flex-col md:flex-row md:justify-center items-center  md:w-5/6 rounded-xl overflow-hidden bg-purple-800">
@@ -20,20 +46,27 @@ export default function Contact() {
    
         </div>
         <div className=" w-full  md:w-1/2 bg-slate-500 p-10">
-          <form action="">
+          <form onSubmit={handleSubmit} autoComplete="off">
             <label htmlFor="" className="block tracking-widest">
               Name
             </label>
             <input
-              type="text"
-              className="block outline-none  bg-slate-500 border-b-2 w-full pt-2"
+              type="text" value={name} name="name" onChange={({target})=>{
+                setName(target.value);
+              }}
+              className="block outline-none  bg-slate-500 border-b-2 border-sky-200 w-full pt-2 focus:border-b-white"
+             autoFocus={false}
             />
             <label htmlFor="" className="mt-8 block tracking-widest">
               Email
             </label>
             <input
-              type="text"
-              className="block outline-none bg-slate-500 border-b-2 w-full pt-2"
+              type="email" value={email} name="email" onChange={({target})=>{
+                setEmail(target.value);
+              }}
+              className="block outline-none bg-slate-500 border-b-2 w-full pt-2 border-sky-200 focus:border-b-white "
+    
+              
             />
             <label htmlFor="" className="mt-8 block tracking-widest">
               Message
@@ -41,7 +74,12 @@ export default function Contact() {
             <textarea
               name="message"
               id="message"
-              className="w-full outline-none bg-slate-500 border-b-2 pt-2"
+              value={message}
+              onChange={({target})=>{
+                setMessage(target.value);
+              }}
+            
+              className="w-full outline-none bg-slate-500 border-b-2 pt-2 border-sky-200 focus:border-b-white "
             ></textarea>
             <button className="float-end w-32 border rounded-full mt-6 h-10 hover:bg-white hover:text-slate-500 transition-all duration-500">
               Submit
