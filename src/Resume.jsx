9e7df369d2010f0d0d7506da5200ff77a5d3f357
Gurshan Singh Aulakh <shan.aulakh29@gmail.com>
2024-10-27 22:15:48 -0700

    added resume in the resume section

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFileArrowDown}from "@fortawesome/free-solid-svg-icons";
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
+import myResume from "./GurshanAulakhResume.pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 export default function Resume() {
-  return (
-    <div className="pt-12 bg-white min-h-screen">
-        <div className="text-center">
-        <button className="bg-blue-600 py-4 px-8 rounded-3xl shadow-2xl tracking-wide relative -top-1 active:-top-0 active:bg-blue-800 transition-all duration-300 ">
-            <div className="inline-block ">Downlad</div> <FontAwesomeIcon icon={faFileArrowDown} /></button>
-        </div>
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
 
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
+  return (
+    <div className="pt-12 bg-white min-h-screen flex flex-col items-center">
+
+      <button className="bg-blue-600 py-4 px-8 rounded-3xl shadow-2xl tracking-wide relative -top-1 active:-top-0 active:bg-blue-800 transition-all duration-300 ">
+        <a href={myResume} download={myResume} className="inline-block ">
+          Downlad
+        </a>{" "}
+        <FontAwesomeIcon icon={faFileArrowDown} />
+      </button>
+
+     
+      <Document file={myResume} className="border mt-2 mb-4 shadow-2xl">
+        <Page pageNumber={1} renderAnnotationLayer={true} />
+      </Document>
+
+      <div className="text-center mb-8">
+      <button className="bg-blue-600 py-4 px-8 rounded-3xl shadow-2xl tracking-wide relative -top-1 active:-top-0 active:bg-blue-800 transition-all duration-300 ">
+        <a href={myResume} download={myResume} className="inline-block ">
+          Downlad
+        </a>{" "}
+        <FontAwesomeIcon icon={faFileArrowDown} />
+      </button>
+      </div>
     </div>
-  )
+  );
 }
