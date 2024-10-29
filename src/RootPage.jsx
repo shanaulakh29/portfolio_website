4fb54d635dc65157894f5dc786c4bb8b30baa765
Gurshan Singh Aulakh <shan.aulakh29@gmail.com>
2024-10-28 22:50:58 -0700

    added error message logic that appears when user enters wrong input in the contact form

--- src/RootPage.jsx
+++ src/RootPage.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
-import WindowWidth from './WindowWidth'
+// import WindowWidth from './WindowWidth'
 export default function RootPage() {
   return (
     <div className="text-white bg-black">
       <Header/>
       <Outlet/>  
       <Footer/>
-      <WindowWidth/>
+      {/* <WindowWidth/> */}
     </div>
   )
 }
